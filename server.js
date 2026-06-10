8c61723ed1bb2e24f5a6bb24f88e42614d3d437f
CarlosBavon <carlosbavon46@gmail.com>
2026-06-10 11:22:47 +0300

    Adding another Client URL

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const {
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "https://stylesbymk.xyz",
   "https://stylebymk.vercel.app",
+  "https://stylesbymk.vercel.app",
   "https://stylebymk-back.onrender.com",
 ];
 
